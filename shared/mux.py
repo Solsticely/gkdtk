d508678c61d246ee977ee963672c9fc74fbc6314
Solsticely <96320174+Solsticely@users.noreply.github.com>
2026-05-30 14:04:53 -0700

    Improve mux script, reduce code size

--- shared/mux.py
+++ shared/mux.py
@@ -1,6 +1,5 @@
 import sys
 import time
-from typing import Literal
 import asyncio as aio
 
 start_time = time.time_ns()
@@ -31,22 +30,46 @@ def pack(proto: bytes, body: bytes | str) -> bytes:
     return proto+('%02x' % len(body)).encode("ascii")+body+b"\n"
 
 
-# Packet types
+# Packet prototypes
 PKS, PKD = b"S", b"d"
+ALNUM_PROTOS = {bytes([i]) for i in b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"}
 
 VERSION = "0.01"
 
-SYNC_MAGIC_1 = pack(
-    PKS,
-    "What would you do for a Klondike bar? Say, "+VERSION
-)
-SYNC_MAGIC_2 = pack(
-    PKS,
-    VERSION
-)
+SYNC_MAGIC_1 = (PKS, "What would you do for a Klondike bar? Say, "+VERSION)
+SYNC_MAGIC_2 = (PKS, VERSION)
 
 global stdin, stdout, is_client, role, debug
 stdin, stdout, debug = None, None, False
+
+
+async def unpack() -> (bytes, bytes):
+    proto = await read(1)
+    if proto not in ALNUM_PROTOS:
+        raise DesyncError("Got invalid packet prototype %s" % proto)
+
+    try:
+        pkt_size = int(await read(2), 16)
+    except ValueError as e:
+        raise DesyncError("Got invalid packet length: "+repr(e))
+    assert 0 <= pkt_size <= 255
+
+    body = await read(pkt_size)
+    delim = await read(1)
+    if delim != b"\n":
+        raise DesyncError(
+            "Expected packet to end with a line feed, got %s instead" % delim)
+
+    return (proto, body)
+
+
+async def assert_packet(proto: bytes, body: bytes | str):
+    if isinstance(body, str):
+        body = body.encode("utf8")
+    packet = await unpack()
+    if packet != (proto, body):
+        raise DesyncError("Expected packet %s, got %s" %
+                          ((proto, body), packet))
 
 
 # Credit: https://stackoverflow.com/q/64303607 CC-BY-SA 4.0
@@ -76,14 +99,6 @@ async def clear_read_buffer(timeout: float = .05):
         pass
 
 
-async def read_match(text: bytes):
-    for char in text:
-        new = await read(1)
-        if new[0] != char:
-            msg = "Expected %s, got %s" % (repr(bytes([char])), repr(new))
-            raise DesyncError(msg)
-
-
 async def write(x):
     if debug:
         bytes_repr = repr(bytes(x.replace(b"\n", b"")))[2:-1]
@@ -96,11 +111,12 @@ async def writep(*args, **kwargs):
     return await write(pack(*args, **kwargs))
 
 
-async def resync(noclear: bool = False, *args, **kwargs):
+async def resync(*args, **kwargs):
     global is_client
 
     timeout = 8 if is_client else 2
     max_tries = 3 if is_client else 10
+    try_resync = try_resync_with_server if is_client else try_resync_as_server
 
     if is_client:
         await write(b"CLIENT IS OUT OF SYNC\n")
@@ -110,14 +126,7 @@ async def resync(noclear: bool = False, *args, **kwargs):
 
         try:
             async with aio.timeout(timeout):
-                if is_client:
-                    await try_resync_with_server(*args, **kwargs)
-                else:
-                    await try_resync_as_server(
-                        *args,
-                        noclear=noclear,
-                        **kwargs
-                    )
+                await try_resync(*args, **kwargs)
 
             await writep(
                 PKD,
@@ -128,22 +137,21 @@ async def resync(noclear: bool = False, *args, **kwargs):
 
         except (DesyncError, TimeoutError) as e:
             log("Resync failed:", repr(e))
-            if is_client and not noclear:
+            if is_client:
                 await clear_read_buffer()
-            elif not is_client:
+            else:
                 await aio.sleep(.2)
 
     log("Out of tries for resyncing")
 
 
-async def try_resync_as_server(noclear: bool = False):
-    if not noclear:
-        await clear_read_buffer()
+async def try_resync_as_server():
+    await clear_read_buffer()
 
-    await write(b"YOU ARE OUT OF SYNC\n"+b"0"*764+pack(PKS, "hi"))
-    await read_match(pack(PKS, "icu"))
-    await write(SYNC_MAGIC_1)
-    await read_match(SYNC_MAGIC_2)
+    await write(b"YOU ARE OUT OF SYNC\n"+b"0"*764+b"1"+pack(PKS, "hi"))
+    await assert_packet(PKS, "icu")
+    await writep(*SYNC_MAGIC_1)
+    await assert_packet(PKS, VERSION)
 
 
 async def try_resync_with_server():
@@ -151,16 +159,61 @@ async def try_resync_with_server():
         if await read(1) == b'0':
             break
 
-    await read_match(b"0"*59)
+    if await read(59) != b"0"*59:
+        raise DesyncError(
+            "Failed to resync, started resync too late, waiting for next cycle"
+        )
 
     for i in range(800):
-        if await read(1) == b'S':
+        if await read(1) == b'1':
             break
 
-    await read_match(pack(PKS, "hi")[1:])
+    await assert_packet(PKS, "hi")
     await writep(PKS, "icu")
-    await read_match(SYNC_MAGIC_1)
-    await write(SYNC_MAGIC_2)
+    await assert_packet(*SYNC_MAGIC_1)
+    await writep(PKS, VERSION)
+
+
+async def listen_client():
+    while True:
+        await resync()
+        await writep(PKD, "desync_for_fun")
+
+        try:
+            pass
+        except DesyncError as e:
+            log("Desynced!", e)
+            continue
+
+        return log("Exiting!")
+
+    # Todo list for client:
+    # TODO: maybe listen on a provided file instead of STDIO
+    # TODO: daemonize, and open a port for commands/connections
+    # TODO: implement a little packet packer and unpacker for the connections port
+    # TODO: create host/gkd python file
+    # TODO: implement really basic shell command through the connections port
+    # TODO: maybe unify packet daemon for both client and server
+    # TODO: implement socket tunneling
+
+
+async def listen_server():
+    while True:
+        await resync()
+
+        try:
+            pass
+        except DesyncError as e:
+            log("Desynced!", e)
+            continue
+
+        return log("Exiting!")
+
+    # Todo list for server:
+    # TODO: implement really basic shell command
+    # TODO: create host/gkd python file and implement shell with it
+    # TODO: connect to 9p server?
+    # TODO: maybe unify packet daemon for both client and server
 
 
 async def main():
@@ -170,7 +223,12 @@ async def main():
         debug = True
     is_client = {"client": True, "server": False}[role]
     stdin, stdout = await get_streams()
-    await resync()
+
+    if is_client:
+        await listen_client()
+    else:
+        await listen_server()
+        
 
 if __name__ == "__main__":
     aio.run(main())
